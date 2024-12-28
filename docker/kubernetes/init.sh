1e075e4af8a774aa72bbc13f2cbf678045f0016f
wanglei4687 <wanglei4687@gmail.com>
2024-12-29 04:06:11 +0800

    add golang version config

--- docker/kubernetes/init.sh
+++ docker/kubernetes/init.sh
@@ -3,14 +3,21 @@
 set -e
 
 function install_golang() {
-    sudo wget https://go.dev/dl/go1.23.3.linux-amd64.tar.gz 
-    sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.23.3.linux-amd64.tar.gz
-    export PATH=$PATH:/usr/local/go/bin
+  sudo wget https://go.dev/dl/go$1.linux-amd64.tar.gz 
+  sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go$1.linux-amd64.tar.gz
+  export PATH=$PATH:/usr/local/go/bin
+}
+
+
+## Using proxy https://ghgo.xyz/ 
+function install_homebrew() {
+  /bin/bash -c "$(curl -fsSL https://ghp.ci/https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 }
 
 function clean() {
-    sudo rm -rf go1.23.3.linux-amd64.tar.gz
+  sudo rm -rf go$1.linux-amd64.tar.gz
 }
 
-install_golang
-clean
+install_homebrew
+install_golang $1
+clean $1
